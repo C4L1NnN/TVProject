a4e0d8d441ac73d4a8334d3e7b8cc2e20404e492
C4L1NnN <calin.ganta2003@gmial.com>
2024-03-07 01:35:03 +0200

    Added About Section

--- TVProject.Web/App_Start/BundleConfig.cs
+++ TVProject.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,7 @@ namespace TVProject.Web.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //<-HOME->
             //bootstrap core css
             bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Vendors/css/bootstrap.css", new CssRewriteUrlTransform()));
 
@@ -24,8 +25,25 @@ namespace TVProject.Web.App_Start
             bundles.Add(new StyleBundle("~/bundles/responsive/css").Include("~/Vendors/css/responsive.css", new CssRewriteUrlTransform()));
 
 
+
+            //<-ABOUT->
+            //bootsrtap core css 
+            bundles.Add(new StyleBundle("~/bundles/bootstrap-About/css").Include("~/Vendors/css/bootstrap.css", new CssRewriteUrlTransform()));
+            
+            //font-awesome style
+            bundles.Add(new StyleBundle("~/bundles/font-awesome-About/css").Include("~/Vendors/css/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            //Custom styles for this template 
+            bundles.Add(new StyleBundle("~/bundles/style-About/css").Include("~/Vendors/css/style.css", new CssRewriteUrlTransform()));
+
+            // responsive style
+            bundles.Add(new StyleBundle("~/bundles/responsive-About/css").Include("~/Vendors/css/responsive.css", new CssRewriteUrlTransform()));
+
+
+
             //--SCRIPTS--
 
+            //<-HOME->
             //jQery
             bundles.Add(new ScriptBundle("~/bundles/jqery/js").Include("~/Vendors/js/jquery-3.4.1.min.js"));
             
@@ -34,6 +52,17 @@ namespace TVProject.Web.App_Start
             
             //custom js
             bundles.Add(new ScriptBundle("~/bundles/custom/js").Include("~/Vendors/js/custom.js"));
+
+
+            //<-ABOUT->
+            //jQery-About
+            bundles.Add(new ScriptBundle("~/bundles/jqery-about/js").Include("~/Vendors/js/jquery-3.4.1.min.js"));
+
+            //bootstrap-About
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap-about/js").Include("~/Vendors/js/bootstrap.js"));
+
+            //custom-About
+            bundles.Add(new ScriptBundle("~/bundles/custom-about/js").Include("~/Vendors/js/custom.js"));
 
         }
     }
