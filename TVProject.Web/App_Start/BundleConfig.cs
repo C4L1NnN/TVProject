a9d2fa7e84f85ca6701ba9dd0df426efb977ac15
calin.ganta2003@gmail.com <calin.ganta2003@gmail.com>
2024-03-07 12:51:10 +0200

    Modoficarea About Page

--- TVProject.Web/App_Start/BundleConfig.cs
+++ TVProject.Web/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace TVProject.Web.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //<-HOME->
+            //--CSS--
+
             //bootstrap core css
             bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Vendors/css/bootstrap.css", new CssRewriteUrlTransform()));
 
@@ -25,25 +26,8 @@ namespace TVProject.Web.App_Start
             bundles.Add(new StyleBundle("~/bundles/responsive/css").Include("~/Vendors/css/responsive.css", new CssRewriteUrlTransform()));
 
 
-
-            //<-ABOUT->
-            //bootsrtap core css 
-            bundles.Add(new StyleBundle("~/bundles/bootstrap-About/css").Include("~/Vendors/css/bootstrap.css", new CssRewriteUrlTransform()));
-            
-            //font-awesome style
-            bundles.Add(new StyleBundle("~/bundles/font-awesome-About/css").Include("~/Vendors/css/font-awesome.min.css", new CssRewriteUrlTransform()));
-
-            //Custom styles for this template 
-            bundles.Add(new StyleBundle("~/bundles/style-About/css").Include("~/Vendors/css/style.css", new CssRewriteUrlTransform()));
-
-            // responsive style
-            bundles.Add(new StyleBundle("~/bundles/responsive-About/css").Include("~/Vendors/css/responsive.css", new CssRewriteUrlTransform()));
-
-
-
             //--SCRIPTS--
 
-            //<-HOME->
             //jQery
             bundles.Add(new ScriptBundle("~/bundles/jqery/js").Include("~/Vendors/js/jquery-3.4.1.min.js"));
             
@@ -52,17 +36,6 @@ namespace TVProject.Web.App_Start
             
             //custom js
             bundles.Add(new ScriptBundle("~/bundles/custom/js").Include("~/Vendors/js/custom.js"));
-
-
-            //<-ABOUT->
-            //jQery-About
-            bundles.Add(new ScriptBundle("~/bundles/jqery-about/js").Include("~/Vendors/js/jquery-3.4.1.min.js"));
-
-            //bootstrap-About
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-about/js").Include("~/Vendors/js/bootstrap.js"));
-
-            //custom-About
-            bundles.Add(new ScriptBundle("~/bundles/custom-about/js").Include("~/Vendors/js/custom.js"));
 
         }
     }
